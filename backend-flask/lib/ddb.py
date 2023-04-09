f564128b33d88b1bb08641215f8c43c2fea4f8b2
jackzzy72 <jackzzy2018@gmail.com>
2023-04-09 23:34:29 +0000

    fix ISO Time issue

--- backend-flask/lib/ddb.py
+++ backend-flask/lib/ddb.py
@@ -84,7 +84,7 @@ class Ddb:
 
     message_group_uuid = str(uuid.uuid4())
     message_uuid = str(uuid.uuid4())
-    now = datetime.now(timezone.utc).astimezone().isoformat()
+    now = datetime.now().isoformat()
     last_message_at = now
     created_at = now
     print('== create_message_group.2')
@@ -141,8 +141,7 @@ class Ddb:
       print(e)
 
   def create_message(client,message_group_uuid, message, my_user_uuid, my_user_display_name, my_user_handle):
-    now = datetime.now(timezone.utc).astimezone().isoformat()
-    created_at = now
+    created_at = datetime.now().isoformat()
     message_uuid = str(uuid.uuid4())
 
     record = {
